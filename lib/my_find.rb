93dab9d3af525c50425f0c8dbc8274ad9fe9d28c
Hannah Malm <hannahemalm616@gmail.com>
2020-12-01 00:41:56 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,7 +3,12 @@ require 'pry'
 def my_find(collection)
 i = 0  
 while i < collection.length 
-#for each element in the collection, yield the block
+#for each element in the collection, yield the block. When this code is run and it hits the yield line, it is going to send whatever is passed in as the argument to the block 
+  if 
+    yield (collection[i])
+    return
+    collection[i]
+  end
   i = i + 1 
 end
 end 
